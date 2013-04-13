4735cada6f966e8685fe09bb1a96086dccc9efef
Michael Bevz <myb@appfellas.co>
2013-04-13 17:22:28 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gokoko::Application.config.secret_token = 'e4f3a69d7f838a0c6aacad1ff4f75d0e15c921a4da10473d9a3883903fd39748f91cbfb0e83536e3f51056cf3e1c49d03ad436236b84d2128acd43573090dba4'
